c8df6268afc04840982aaf48eaedf993a41d37c7
douglas-cavalcante <douglascavalcante2803@gmail.com.br>
2023-06-05 20:49:00 -0300

    adiciona o codigo do primeiro exemplo

---
+++ mediaaluno.js
@@ -0,0 +1,11 @@
+/*
+var nome = "douglas"
+console.log("Menu nome é " + nome)
+*/
+
+var nota1 = Number(prompt("Digite a nota 1"))
+var nota2 = Number(prompt("Digite a nota 2"))
+var nota3 = Number(prompt("Digite a nota 3"))
+var nota4 = Number(prompt("Digite a nota 4"))
+
+console.log( (nota1 + nota2 + nota3 + nota4) / 4)
